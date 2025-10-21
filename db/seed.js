2c2e869ab94f6c30c79de6aacafe5140165a85cd
Josiah Sayles <Josiah.Sayles@gmail.com>
2025-10-21 16:32:41 -0600

    completed the first part of Fullstack employees workshop

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "./queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +7,12 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: "Employee" + i,
+      birthday: faker.date.past({ years: 20 }),
+      salary: faker.number.int({ min: 50000, max: 200000 }),
+    };
+    await createEmployee(employee);
+  }
 }
